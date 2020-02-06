938a37e3616a0ea749c90c48e6b18ae93e9fe99f
pubuduudara <pubuduudara7@gmail.com>
2020-02-06 09:10:51 +0530

    update

--- Backend/Uplaod_to_database/firebase/firebase.ino
+++ Backend/Uplaod_to_database/firebase/firebase.ino
@@ -31,15 +31,11 @@ FirebaseJson json;
 
 void printResult(FirebaseData &data);
 
-void setup()
-{
-
+void setup() {
   Serial.begin(115200);
-
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(300);
   }
@@ -56,239 +52,58 @@ void setup()
   //tiny, small, medium, large and unlimited.
   //Size and its write timeout e.g. tiny (1s), small (10s), medium (30s) and large (60s).
   Firebase.setwriteSizeLimit(firebaseData, "tiny");
-
-  /*
-    This option allows get and delete functions (PUT and DELETE HTTP requests) works for device connected behind the
-    Firewall that allows only GET and POST requests.
-
-    Firebase.enableClassicRequest(firebaseData, true);
-  */
-
-  //  String path = "/Test";
-  //
-  //  Serial.println("------------------------------------");
-  //  Serial.println("Set double test...");
-  //
-  //  for (uint8_t i = 0; i < 10; i++)
-  //  {
-  //    //Also can use Firebase.set instead of Firebase.setDouble
-  //    if (Firebase.setDouble(firebaseData, path + "/Double/Data" + (i + 1), ((i + 1) * 10) + 0.123456789))
-  //    {
-  //      Serial.println("PASSED");
-  //      Serial.println("PATH: " + firebaseData.dataPath());
-  //      Serial.println("TYPE: " + firebaseData.dataType());
-  //      Serial.println("ETag: " + firebaseData.ETag());
-  //      Serial.print("VALUE: ");
-  //      printResult(firebaseData);
-  //      Serial.println("------------------------------------");
-  //      Serial.println();
-  //    }
-  //    else
-  //    {
-  //      Serial.println("FAILED");
-  //      Serial.println("REASON: " + firebaseData.errorReason());
-  //      Serial.println("------------------------------------");
-  //      Serial.println();
-  //    }
-  //  }
-  //
-  //  Serial.println("------------------------------------");
-  //  Serial.println("Get double test...");
-  //
-  //  for (uint8_t i = 0; i < 10; i++)
-  //  {
-  //    //Also can use Firebase.get instead of Firebase.setInt
-  //    if (Firebase.getInt(firebaseData, path + "/Double/Data" + (i + 1)))
-  //    {
-  //      Serial.println("PASSED");
-  //      Serial.println("PATH: " + firebaseData.dataPath());
-  //      Serial.println("TYPE: " + firebaseData.dataType());
-  //      Serial.println("ETag: " + firebaseData.ETag());
-  //      Serial.print("VALUE: ");
-  //      printResult(firebaseData);
-  //      Serial.println("------------------------------------");
-  //      Serial.println();
-  //    }
-  //    else
-  //    {
-  //      Serial.println("FAILED");
-  //      Serial.println("REASON: " + firebaseData.errorReason());
-  //      Serial.println("------------------------------------");
-  //      Serial.println();
-  //    }
-  //  }
-  //
-  //  Serial.println("------------------------------------");
-  //  Serial.println("Push integer test...");
-  //
-  //  for (uint8_t i = 0; i < 5; i++)
-  //  {
-  //    //Also can use Firebase.push instead of Firebase.pushInt
-  //    if (Firebase.pushInt(firebaseData, path + "/Push/Int", (i + 1)))
-  //    {
-  //      Serial.println("PASSED");
-  //      Serial.println("PATH: " + firebaseData.dataPath());
-  //      Serial.print("PUSH NAME: ");
-  //      Serial.println(firebaseData.pushName());
-  //      Serial.println("ETag: " + firebaseData.ETag());
-  //      Serial.println("------------------------------------");
-  //      Serial.println();
-  //    }
-  //    else
-  //    {
-  //      Serial.println("FAILED");
-  //      Serial.println("REASON: " + firebaseData.errorReason());
-  //      Serial.println("------------------------------------");
-  //      Serial.println();
-  //    }
-  //  }
-  //
-  //  Serial.println("------------------------------------");
-  //  Serial.println("Push JSON test...");
-  //
-  //  for (uint8_t i = 5; i < 10; i++)
-  //  {
-  //
-  //    json.clear().add("Data" + String(i + 1), i + 1);
-  //
-  //    //Also can use Firebase.push instead of Firebase.pushJSON
-  //    //Json string is not support in v 2.6.0 and later, only FirebaseJson object is supported.
-  //    if (Firebase.pushJSON(firebaseData, path + "/Push/Int", json))
-  //    {
-  //      Serial.println("PASSED");
-  //      Serial.println("PATH: " + firebaseData.dataPath());
-  //      Serial.print("PUSH NAME: ");
-  //      Serial.println(firebaseData.pushName());
-  //      Serial.println("ETag: " + firebaseData.ETag());
-  //      Serial.println("------------------------------------");
-  //      Serial.println();
-  //    }
-  //    else
-  //    {
-  //      Serial.println("FAILED");
-  //      Serial.println("REASON: " + firebaseData.errorReason());
-  //      Serial.println("------------------------------------");
-  //      Serial.println();
-  //    }
-  //  }
-  //
-  //  Serial.println("------------------------------------");
-  //  Serial.println("Update test...");
-  //
-  //  for (uint8_t i = 0; i < 5; i++)
-  //  {
-  //
-  //    json.set("Data" + String(i + 1), i + 5.5);
-  //
-  //    if (Firebase.updateNode(firebaseData, path + "/float", json))
-  //    {
-  //      Serial.println("PASSED");
-  //      Serial.println("PATH: " + firebaseData.dataPath());
-  //      Serial.println("TYPE: " + firebaseData.dataType());
-  //      //No ETag available
-  //      Serial.print("VALUE: ");
-  //      printResult(firebaseData);
-  //      Serial.println("------------------------------------");
-  //      Serial.println();
-  //    }
-  //    else
-  //    {
-  //      Serial.println("FAILED");
-  //      Serial.println("REASON: " + firebaseData.errorReason());
-  //      Serial.println("------------------------------------");
-  //      Serial.println();
-  //    }
-  //  }
-
-  String path = "/Test1";
-  Firebase.setDouble(firebaseData, path + "/Double2/Data1", 100);
 }
 
-void printResult(FirebaseData &data)
-{
+String data_base = "batches";
+//String batch_number = "batch_number";
+int batch_number = 0;
+//String roll_number = "roll_number";
+//int roll_id = 1;
+String width = "width";
+String Tklen = "Tklen";
+String plies = "plies";
+String damage = "damage";
+String joint = "joint";
+String ends = "ends";
+String del = "del";
 
-  if (data.dataType() == "int")
-    Serial.println(data.intData());
-  else if (data.dataType() == "float")
-    Serial.println(data.floatData(), 5);
-  else if (data.dataType() == "double")
-    printf("%.9lf\n", data.doubleData());
-  else if (data.dataType() == "boolean")
-    Serial.println(data.boolData() == 1 ? "true" : "false");
-  else if (data.dataType() == "string")
-    Serial.println(data.stringData());
-  else if (data.dataType() == "json")
-  {
-    Serial.println();
-    FirebaseJson &json = data.jsonObject();
-    //Print all object data
-    Serial.println("Pretty printed JSON data:");
-    String jsonStr;
-    json.toString(jsonStr, true);
-    Serial.println(jsonStr);
-    Serial.println();
-    Serial.println("Iterate JSON data:");
-    Serial.println();
-    size_t len = json.iteratorBegin();
-    String key, value = "";
-    int type = 0;
-    for (size_t i = 0; i < len; i++)
-    {
-      json.iteratorGet(i, type, key, value);
-      Serial.print(i);
-      Serial.print(", ");
-      Serial.print("Type: ");
-      Serial.print(type == JSON_OBJECT ? "object" : "array");
-      if (type == JSON_OBJECT)
-      {
-        Serial.print(", Key: ");
-        Serial.print(key);
-      }
-      Serial.print(", Value: ");
-      Serial.println(value);
-    }
-    json.iteratorEnd();
-  }
-  else if (data.dataType() == "array")
-  {
-    Serial.println();
-    //get array data from FirebaseData using FirebaseJsonArray object
-    FirebaseJsonArray &arr = data.jsonArray();
-    //Print all array values
-    Serial.println("Pretty printed Array:");
-    String arrStr;
-    arr.toString(arrStr, true);
-    Serial.println(arrStr);
-    Serial.println();
-    Serial.println("Iterate array values:");
-    Serial.println();
-    for (size_t i = 0; i < arr.size(); i++)
-    {
-      Serial.print(i);
-      Serial.print(", Value: ");
+void create_new_batch() {
+  batch_number += 1;
+  Firebase.setDouble(firebaseData, data_base + "/" + String(batch_number), 0);
+}
 
-      FirebaseJsonData &jsonData = data.jsonData();
-      //Get the result data from FirebaseJsonArray object
-      arr.get(jsonData, i);
-      if (jsonData.typeNum == JSON_BOOL)
-        Serial.println(jsonData.boolValue ? "true" : "false");
-      else if (jsonData.typeNum == JSON_INT)
-        Serial.println(jsonData.intValue);
-      else if (jsonData.typeNum == JSON_DOUBLE)
-        printf("%.9lf\n", jsonData.doubleValue);
-      else if (jsonData.typeNum == JSON_STRING ||
-               jsonData.typeNum == JSON_NULL ||
-               jsonData.typeNum == JSON_OBJECT ||
-               jsonData.typeNum == JSON_ARRAY)
-        Serial.println(jsonData.stringValue);
-    }
-  }
+void create_new_roll(int roll_id ) {
+  Firebase.setDouble(firebaseData, data_base + "/" + String(batch_number) + "/" + String(roll_id), 0);
+}
+
+void update_plies(int roll_id, int value) {
+  Firebase.setDouble(firebaseData, data_base + "/" + String(batch_number) + "/" + String(roll_id) + "/number_of_plies", value);
+}
+
+void update_layer_length(int roll_id, int value) {
+  Firebase.setDouble(firebaseData, data_base + "/" + String(batch_number) + "/" + String(roll_id) + "/layer_length", value);
+}
+void update_layer_length(int roll_id, int value) {
+  Firebase.setDouble(firebaseData, data_base + "/" + String(batch_number) + "/" + String(roll_id) + "/layer_length", value);
+}
+void update_total_damage_length(int roll_id, int value) {
+  Firebase.setDouble(firebaseData, data_base + "/" + String(batch_number) + "/" + String(roll_id) + "/total_damage_length", value);
+}
+
+void update_total_overlapped_length(int roll_id, int value) {
+  Firebase.setDouble(firebaseData, data_base + "/" + String(batch_number) + "/" + String(roll_id) + "/total_overlapped_length", value);
 }
 
 void loop()
 {
   Serial.println("running loop");
   String path = "/Test1";
-  Firebase.setDouble(firebaseData, path + "/Double2/Data1", 101);
+  //Firebase.setDouble(firebaseData, path + "/Double2/Data1", 101);
+  create_new_batch();
+  create_new_roll(1);
+  update_plies(1, 10);
+  update_layer_length(1, 20);
+  create_new_roll(2);
+  update_plies(2, 100);
   delay(300);
 }
